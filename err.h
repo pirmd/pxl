a0a280a8826cde8f6fe0f108df84a45346f17f7b
Πrmd <pirmd@users.noreply.github.com>
2026-08-23 19:11:15 +0200

    feat: add a minimal pxl_err_to_string

--- err.h
+++ err.h
@@ -9,4 +9,17 @@ typedef enum {
     PXL_E_BACKEND_FRAME,
 } pxl_err_t;
 
+/* Return a static string describing the error code. */
+static inline const char *
+pxl_err_to_string(pxl_err_t err) {
+    switch (err) {
+    case PXL_SUCCESS:          return "Success";
+    case PXL_E_INVALID_PARAM:  return "Invalid parameter";
+    case PXL_E_OUT_OF_MEM:     return "Out of memory";
+    case PXL_E_BACKEND_INIT:   return "Backend initialization failed";
+    case PXL_E_BACKEND_FRAME:  return "Backend frame failed";
+    default:                   return "Unknown error";
+    }
+}
+
 #endif /* PXL_ERR_H */
